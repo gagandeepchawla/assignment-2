1d37a9114cdbc095860b710cfb4db61016e46cca
gagandeep <Gagandeep.Chawla@IB-SEZ-PC-028.infobeans.in>
2017-11-20 18:38:45 +0530

    # Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.

---
+++ sum.py
@@ -0,0 +1,22 @@
+# Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
+class twosum:
+    def sum(self,number1, number2):
+        add = number1 + number2
+        return add
+
+    def check(self,number1, number2):
+        check1 = self.sum(number1, number2)
+        if check1 in range(10, 19):
+            print 20
+        else:
+            print check1
+
+    # def insert(self):
+    #     first = raw_input("Enter the First Number\n")
+    #     second = raw_input("Enter the second Number\n")
+    #     self.check(first, second)
+
+n = twosum()
+n.check(3, 4)
+n.check(9, 4)
+n.check(10, 11)
